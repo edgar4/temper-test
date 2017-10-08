08cf96face6437ef9e6c526b827a02271de0798d
edgar chris <edgarchris4@gmail.com>
2017-10-08 17:47:17 +0300

    add plot series map

--- src/TemperBundle/Controller/DefaultController.php
+++ src/TemperBundle/Controller/DefaultController.php
@@ -9,13 +9,23 @@ class DefaultController extends Controller
     public function indexAction()
     {
 
-
+        $series = array();
         $temperCohorts = $this->get('temper.user.cohort');
-        $temperCohorts->get('2016-07-01', '2016-08-12');
-        exit;
+        $cohorts = $temperCohorts->get('2016-07-01', '2016-08-12');
+
+        //echo "<pre>";
+
+
+        foreach ($cohorts as $chart) {
+            $onboarding = (int)$chart['onboarding_perentage'];
+            $name = str_replace("2016", "week ", $chart['yw']);
+            $chartCat[] = $name;
+            $series[] = array("name" => $name, "data" => array($onboarding,$name));
+
+        }
 
         return $this->render('TemperBundle:Default:index.html.twig', array(
-            'cohorts' => $temperCohorts->get()
+            'chart' => json_encode($series),
         ));
     }
 
